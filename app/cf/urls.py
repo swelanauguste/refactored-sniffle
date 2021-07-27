721899944d49bfcac32f884d29a15da11b2a4d73
swelanauguste@gmail.com <swelanauguste@gmail.com>
2021-07-27 19:05:00 -0400

    27072021

--- app/cf/urls.py
+++ app/cf/urls.py
@@ -6,5 +6,7 @@ from mixin.assets import IndexView
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("applicants/", include("applicants.urls", namespace="applicants")),
+    path("applications/", include("applications.urls", namespace="applications")),
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
 ]
